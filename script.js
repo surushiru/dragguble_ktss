4534ebe9d00ee2021f66a83484007d0fd615261a
surushiru <sugunitobu@gmail.com>
2023-09-28 17:30:27 +0800

    增加選擇層次

--- script.js
+++ script.js
@@ -17,22 +17,25 @@ const move = function (element) {
   }
 
   const elements = document.querySelectorAll(".element");
-
-  elements.forEach(element => {
+  var r = 0;
+  elements.forEach((element) => {
+    
     element.addEventListener("mousedown", () => {
       element.style.position = "absolute";
       chooseElement = element;
-
+      chooseElement.style.zIndex = r++;
       document.onmousemove = (e) => {
         var x = e.pageX;
         var y = e.pageY;
-        chooseElement.style.left = x -60 + "px";
-        chooseElement.style.top = y -60 + "px";
+        chooseElement.style.left = x - 60 + "px";
+        chooseElement.style.top = y - 60 + "px";
+        
         // console.log(chooseElement);
       };
     });
   });
-  document.onmouseup = function (e) {
-    chooseElement = null;
-  };
 };
+document.onmouseup = function (e) {
+  chooseElement = null;
+};
+window.onload = move("element");
